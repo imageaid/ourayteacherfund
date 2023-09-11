7b30757c969de2c612dd0b5ce94d9bcdbb534241
Craig Kaminsky <imageaid@gmail.com>
2023-09-11 17:29:51 -0600

    Add recent donations and back button display on welcome page
    In the WelcomeController, the index method now fetches a list of the most recent donations to be displayed on the welcome page. This provides an overview of the latest donations activity. Additionally, the 'shared/back_button' has been added to the board_members index view, enhancing user navigation functionality.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
+    @donations = Donation.includes(:donor).order(created_at: :desc).limit(6)
     @page = Page.find_by(welcome_content: true)
     @pages = Page.where(navigation: true, welcome_content: false).order(:position)
   end
