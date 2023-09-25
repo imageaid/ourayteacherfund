17ccb0faf282f27a8dcfa0680cb70b5d6e35eef7
Craig Kaminsky <imageaid@gmail.com>
2023-09-25 09:38:00 -0600

    Move SMPT credentials to ENV and production. Development uses local previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,15 +67,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.net',
-    port: 587,
-    domain: ENV['MAIL_DOMAIN'],
-    user_name: ENV['GMAIL_ACCOUNT'],
-    password: ENV['GMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
